3a3569816b876f9a0916a3746a9fb6f27e0c770d
noritama73 <takuma.shioiri.eiko.rbb@gmail.com>
2020-09-21 11:27:15 +0900

    エラーの確認

---
+++ docs/use-child-from-parent/src/components/Child.vue
@@ -0,0 +1,24 @@
+<template>
+    <div v-show="isShow">
+        <p>ここは子コンポーネント</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            isShow: false
+        }
+    },
+    methods: {
+        openChild(){
+            isShow = !isShow
+        }
+    }
+};
+</script>
+
+<style>
+
+</style>
---
+++ docs/use-child-from-parent/src/components/Parent.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="parent">
+            <p>ここは親コンポーネント</p>
+            <button class="button-outline" @click="open">開く</button>
+        </div>
+        <div class="child">
+            <Child ref="child"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import Child from './Child'
+export default {
+    components: {
+        "Child" : Child
+    },
+    methods: {
+        open(){
+            this.$refs.child.openChild()
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
